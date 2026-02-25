0cb4abb1f0b122bedf6d1d97399e2a9c857359bb
NivDantas <dantasniv@gmail.com>
2026-02-25 17:22:30 -0300

    Update Elysia routes.

--- src/modules/posts/index.ts
+++ src/modules/posts/index.ts
@@ -1,13 +1,35 @@
-import { Elysia } from 'elysia'
+import {Elysia, t} from 'elysia'
 import {postService} from './service'
-export const post = new Elysia()
+
+export const postRoutes = new Elysia({prefix: '/posts'})
     .use(postService)
 
 
-    .get('/', 'posts')
-    .get('/posts/:slug', 'Get from slug')
+    .get('/', () => {
+        return 'Get all posts'
+    })
+    .get('/:slug', 'Get from slug')
 
-    .post('/posts', () => {})
-    .put('/posts/:slug', 'PUT POST')
-    .delete('/posts/:slug', 'Delete POST')
-    .listen(3000)
+    .post('/', ({body}) => {
+        return `Created POST: ${body.title}`
+    }, {
+        body: t.Object({
+            title: t.String(),
+            content: t.String()
+        })
+    })
+    .put('/:slug', ({params: {slug}, body}) => {
+        return `Updated POST: ${slug}`
+    }, {
+        body: t.Object({
+            title: t.Optional(t.String()),
+            content: t.Optional(t.String())
+        })
+    })
+    .delete('/:slug', ({params: {slug}}) => {
+        return `Deleted POST: ${slug}`
+    })
+
+postRoutes.listen(3000, ({hostname, port}) => {
+    console.log(`Post API is running at http://${hostname}:${port}`)
+})
